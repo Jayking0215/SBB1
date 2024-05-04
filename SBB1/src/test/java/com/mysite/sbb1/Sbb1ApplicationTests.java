dc49557bb269615293f9219cdb9178b098eab067
J.J.J <zaesoon2@naver.com>
2024-05-04 22:24:45 +0900

    Hibernate적용

--- SBB1/src/test/java/com/mysite/sbb1/Sbb1ApplicationTests.java
+++ SBB1/src/test/java/com/mysite/sbb1/Sbb1ApplicationTests.java
@@ -1,29 +1,24 @@
 package com.mysite.sbb1;
 
-import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class Sbb1ApplicationTests {
 
-	@Autowired
-	private QuestionRepository questionRepository;
-	
-	@Test
-	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("What is SBB?");
-		q1.setContent("I wanna know what is SBB");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);
-		
-		Question q2 = new Question();
-		q2.setSubject("Question: SpringBoot");
-		q2.setContent("Is id create automatic?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);
-	}
+@SpringBootTest
+class SbbApplicationTests {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    void testJpa() {
+    	Question q = this.questionRepository.findBySubjectAndContent(
+                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+        assertEquals(1, q.getId());
+    }
 }
