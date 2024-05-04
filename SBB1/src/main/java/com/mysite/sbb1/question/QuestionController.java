d8740484ac0efdfd0a7643d5ef090734fb33e452
J.J.J <zaesoon2@naver.com>
2024-05-04 23:11:31 +0900

    /questoin 프리픽스 설정

--- SBB1/src/main/java/com/mysite/sbb1/question/QuestionController.java
+++ SBB1/src/main/java/com/mysite/sbb1/question/QuestionController.java
@@ -6,16 +6,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 	
 	private final QuestionService questionService;
 	
-	@GetMapping("/question/list")
+	@GetMapping("/list")
 //	@ResponseBody
 	public String list(Model model) {
 		List<Question> questionList = this.questionService.getList();
@@ -23,7 +25,7 @@ public class QuestionController {
 		return "question_list";
 	}
 
-	@GetMapping(value = "/question/detail/{id}")
+	@GetMapping(value = "/detail/{id}")
 	public String detail(Model model, @PathVariable("id") Integer id) {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
