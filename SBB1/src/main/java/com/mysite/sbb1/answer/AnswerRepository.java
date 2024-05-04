7b1eddf71d889928457ac4249479c9f20277014c
J.J.J <zaesoon2@naver.com>
2024-05-04 22:32:14 +0900

    package 분리

--- SBB1/src/main/java/com/mysite/sbb1/answer/AnswerRepository.java
+++ SBB1/src/main/java/com/mysite/sbb1/answer/AnswerRepository.java
@@ -1,4 +1,4 @@
-package com.mysite.sbb1;
+package com.mysite.sbb1.answer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- SBB1/src/test/java/com/mysite/sbb1/Sbb1ApplicationTests.java
+++ SBB1/src/test/java/com/mysite/sbb1/Sbb1ApplicationTests.java
@@ -2,11 +2,15 @@ package com.mysite.sbb1;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mysite.sbb1.question.Question;
+import com.mysite.sbb1.question.QuestionRepository;
 
 
 @SpringBootTest
@@ -15,10 +19,11 @@ class SbbApplicationTests {
     @Autowired
     private QuestionRepository questionRepository;
 
+    @Transactional
     @Test
     void testJpa() {
-    	Question q = this.questionRepository.findBySubjectAndContent(
-                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-        assertEquals(1, q.getId());
+    	List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+        Question q = qList.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
     }
 }
